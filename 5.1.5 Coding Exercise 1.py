35faa34ef8fdd2fd23ebb8630e4ca13cc9b30744
ylc0006 <ylc0006@gmail.com>
2017-08-17 14:41:57 +0800

    Add files via upload

---
+++ 5.1.5 Coding Exercise 1.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 17 14:19:10 2017
+
+@author: yan.yl.chen
+"""
+
+#Classes can also have references to other instances of
+#themselves. Consider this Person class, for example, 
+#that allows for an instance of a father and mother
+#to be given in the constructor.
+#
+#Create 3 instances of this class. The first should have 
+#the name "Mr. Burdell" with an age of 53. The second
+#instance should have a name of "Mrs. Burdell" with an age
+#of 53 as well. Finally, make an instance with the name of
+#"George P. Burdell" with an age of 25. This final instance
+#should also have the father attribute set to the instance 
+#of Mr. Burdell, and the mother attribute set to the 
+#instance of Mrs. Burdell. Finally, store the instance of 
+#George P. Burdell in a variable called george_p. (It does
+#not matter what variable names you use for Mr. and Mrs.
+#Burdell.)
+
+class Person:
+    def __init__(self, name, age, father=None, mother=None):
+        self.name = name
+        self.age = age
+        self.father = father
+        self.mother = mother
+
+#Write your code here!
+
+george_p = Person("George P. Burdell", 25, Person("Mr. Burdell", 53), 
+                  Person("Mrs. Burdell",53))
+
+
+
+#The code below will let you test your code. It isn't used
+#for grading, so feel free to modify it. As written, it
+#should print George P. Burdell, Mrs. Burdell, and Mr.
+#Burdell each on a separate line.
+print(george_p.name)
+print(george_p.mother.name)
+print(george_p.father.name)
